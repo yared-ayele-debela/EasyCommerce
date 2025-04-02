0ae679b6941c83035b70d9a15f91a3511ab6a81c
dawit12123 <dawitgirma71@gmail.com>
2025-04-02 03:59:57 +0300

    update data

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    protected $table = 'rooms';
+
+    protected $fillable = [
+        'hotel_id',
+        'room_type',
+        'capacity',
+        'price',
+        'is_available',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'is_available' => 'boolean',
+    ];
+
+    public function hotel()
+    {
+        return $this->belongsTo(Hotel::class);
+    }
 }
