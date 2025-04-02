0228d518f947889e4bcb762dc62480ebab6426d3
dawit12123 <dawitgirma71@gmail.com>
2025-04-02 04:32:08 +0300

    update data

---
+++ app/Models/Amenity.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Amenity extends Model
+{
+    use HasFactory;
+
+    protected $table = 'amenities';
+
+    protected $fillable = [
+        'name',
+        'icon',
+    ];
+
+    public $timestamps = false;
+}
