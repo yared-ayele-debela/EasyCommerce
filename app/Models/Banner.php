9a04e6e0f4a5d2e8c179576b25068542fe85f488
dawit12123 <dawitgirma71@gmail.com>
2025-04-04 02:05:36 +0300

    update the foods

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -10,7 +10,7 @@ class Banner extends Model
     use HasFactory;
 
     protected $fillable = ['service_id', 'product_id', 'image', 'type', 'title', 'alt', 'status'];
-
+    protected $table = 'banner';
     public function service()
     {
         return $this->belongsTo(Service::class);
