319c004d30929a1313a18671a6cdab12dbbc041d
yared-ayele-debela <yareayele67.com>
2025-04-29 09:15:57 -0400

    hidden categories when mobile view

--- resources/views/all_frontend_layouts/header.blade.php
+++ resources/views/all_frontend_layouts/header.blade.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Storage;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant</title>
+    <title>{{ $appsetting['tiltle'] }}</title>
     <link href="{{ asset('/storage/appsettings/'.$appsetting['favicon']) }}" rel="shortcut icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
