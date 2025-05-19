eb3d5cb9886598917312794187447956c2b9ac73
yared-ayele-debela <yareayele67.com>
2025-05-19 01:20:44 -0400

    display out of stock product only related to current logined users or admin

--- app/Http/Controllers/Admin/OutOfStockProductController.php
+++ app/Http/Controllers/Admin/OutOfStockProductController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OutOfStockProductController extends Controller
 {
     //
     public function index()
     {
-        $outOfStockProducts = Product::with('category','brand','group')->whereDoesntHave('attributes') // or use your custom logic
-            ->orWhereHas('attributes', function ($q) {
-                $q->where('stock', '<=', 0);
-            })->paginate(10);
+          $vendor_id = Auth::guard('admin')->user()->vendor_id;
+
+          $outOfStockProducts = Product::with('category','brand','group')->where('vendor_id', $vendor_id)->whereDoesntHave('attributes') // or use your custom logic
+                        ->orWhereHas('attributes', function($q){
+                            $q->where('stock', '<=', 0);
+                     })->paginate(10);
 
         return view('admin.product.index', compact('outOfStockProducts'));
     }
