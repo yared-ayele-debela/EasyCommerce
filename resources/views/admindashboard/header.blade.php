b044c62e077a7359f4b6a007e25268c2b45a5230
yared-ayele-debela <yareayele67.com>
2025-05-24 02:22:08 -0400

    fix display vendor name

--- resources/views/admindashboard/header.blade.php
+++ resources/views/admindashboard/header.blade.php
@@ -52,7 +52,7 @@ use Illuminate\Support\Facades\Auth;
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown"> <img src="{{ Auth::guard('admin')->user()->image }}" alt="Profile" class="rounded-circle"> <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::guard('admin')->name }}</span> </a>
                 <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                     <li class="dropdown-header">
-                        <h6>{{ Auth::guard('admin')->name }}</h6>
+                        <h6>{{ Auth::guard('admin')->user()->name }}</h6>
                     </li>
                     <li>
                         <hr class="dropdown-divider">
