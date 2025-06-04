15a5920e07892b993ea5df7dfc5704f34857b816
yared-ayele-debela <yareayele67.com>
2025-06-04 07:05:56 -0400

    display three product in mobile view

--- resources/views/all_frontend_layouts/partials/product-cards.blade.php
+++ resources/views/all_frontend_layouts/partials/product-cards.blade.php
@@ -1,5 +1,5 @@
 @foreach($auto_scroll_products as $product)
-<div class="col-md-3 col-6 col-lg-2">
+<div class="col-md-3 col-4 col-lg-2">
    <x-restaurant.normal-product-card :product="$product" badge="" bgColor="" />
 </div>
 @endforeach
