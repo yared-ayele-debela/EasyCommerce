2e307da8884de1b93d5fcfbbd9b5f8ebedf41b6b
yared-ayele-debela <yareayele67.com>
2025-07-23 10:08:07 -0400

    update map locations

--- app/Http/Controllers/Restaurant/Frontend/RestaurantController.php
+++ app/Http/Controllers/Restaurant/Frontend/RestaurantController.php
@@ -116,7 +116,7 @@ class RestaurantController extends Controller
         id, name, cover, description, address, rating, start_from, latitude, longitude,delivery_radius,
         (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance
             ", [$latitude, $longitude, $latitude])
-            ->having('distance', '<=', 10) // Restaurants within 10 km
+            ->having('distance', '<=', 30) // Restaurants within 10 km
             ->orderBy('distance', 'asc')
             ->get();
 
