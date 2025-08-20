044ccbff456e528f532d3146df7c6d02a33a7f46
yared-ayele-debela <yaredayele67@gmail.com>
2025-08-20 17:17:15 +0300

    add ecomerce

--- resources/views/components/sample/restaurant/product-card.blade.php
+++ resources/views/components/sample/restaurant/product-card.blade.php
@@ -24,7 +24,7 @@
 
         <a href="{{ url('ecommerce/product/' . encrypt($product->id)) }}">
             <img src="{{ asset('storage/' . $product->product_image) ?? asset('restaurant_frontend/default-image.png') }}"
-                 class="card-img-top p-3 product-image" alt="{{ $product->product_name }}" >
+                 class="card-img-top p-3 product-image" alt="{{ $product->product_name }}" loading="lazy">
         </a>
 
         <div class="card-body p-3">
